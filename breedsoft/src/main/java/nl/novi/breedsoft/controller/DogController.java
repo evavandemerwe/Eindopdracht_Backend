05445b432882808f31728e83ea413ec1dac5ba7c
EvadeJong <Eefjuh.de.jong@gmail.com>
2022-11-19 16:40:43 +0100

    Dogcontroller en repository added

--- breedsoft/src/main/java/nl/novi/breedsoft/controller/DogController.java
+++ breedsoft/src/main/java/nl/novi/breedsoft/controller/DogController.java
@@ -2,89 +2,94 @@ package nl.novi.breedsoft.controller;
 
 import nl.novi.breedsoft.model.animal.Dog;
 import nl.novi.breedsoft.model.animal.enumerations.Breed;
+import nl.novi.breedsoft.repository.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import java.net.URI;
+
 @RestController
+@RequestMapping("dogs")
 public class DogController {
 
-    private ArrayList<Dog> dogs;
-
-    public DogController(){
-        dogs = new ArrayList<>();
-        Dog Saar = new Dog();
-        Saar.setName("Saartje");
-        Saar.setBreed(Breed.Dachschund);
-        Saar.setDateOfBirth(LocalDate.now().minusYears(5));
-        dogs.add(Saar);
-    }
+    @Autowired
+    public DogRepository dogRepository;
 
     //Get mapping voor alle honden in de database
-    @GetMapping("/dogs")
-    public ResponseEntity<Object> getAllDogs(){
-        if(dogs.size() >= 0) {
-            return new ResponseEntity<>(dogs, HttpStatus.OK);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @GetMapping("")
+    public ResponseEntity<Iterable<Dog>> getAllDogs() {
+
+        if (dogRepository.count() > 0) {
+            return ResponseEntity.ok(dogRepository.findAll());
+        } else {
+            return ResponseEntity.noContent().build();
         }
     }
 
     //Get mapping voor een specifieke hond in een database op basis van ID
-    @GetMapping("/dogs/{id}")
-    public  ResponseEntity<Object> getOneDog(@PathVariable int id){
-        if(id < dogs.size()){
-            Dog result = dogs.get(id);
-            return new ResponseEntity<>(result, HttpStatus.OK);
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneDog(@PathVariable long id) {
+        if (dogRepository.existsById(id)) {
+
+            return ResponseEntity.ok(dogRepository.findById(id));
         }
-        return new ResponseEntity<>("ID not found in database", HttpStatus.NOT_FOUND);
+        return ResponseEntity.notFound().build();
     }
 
     //Get mapping voor alle honden met de opgegeven naam in de database
-    @GetMapping("/dogs/name={name}")
-    public  ResponseEntity<Object> getDogsWithName(@PathVariable String name){
-        ArrayList <Dog> result = new ArrayList<>();
-            for(Dog dog : dogs) {
-                if (dog.getName().equalsIgnoreCase(name)) {
-                    result.add(dog);
-                }
-            }
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    @GetMapping("/name={name}")
+    public ResponseEntity<Object> getDogsWithName(@PathVariable String name) {
+
+        if (dogRepository.findByNameContaining(name) != null) {
+            return ResponseEntity.ok(dogRepository.findByNameContaining(name));
+        }
+        return ResponseEntity.notFound().build();
     }
 
     //Post mapping voor het toevoegen van een nieuwe hond in de database
-    @PostMapping("/dogs")
-    public ResponseEntity<Dog> addDog(@RequestBody Dog dog) {
-        if(dog != null) {
-            dogs.add(dog);
-            return new ResponseEntity<>(dog, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    @PostMapping("")
+    public ResponseEntity<Object> addDog(@RequestBody Dog dog) {
+        Dog savedDog = dogRepository.save(dog);
+
+        //Teruggeven van het nieuwe resource path in de header
+        URI uri = URI.create(ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/dogs/" + savedDog.getId())
+                .toUriString());
+        return ResponseEntity.created(uri).body("Dog created: " + savedDog.getName() + ".");
     }
 
     //Put mapping voor aanpassen van een hond met het opgegeven id de een database
-    @PutMapping("/dogs/{id}")
-    public ResponseEntity<Object> editDog(@PathVariable int id, @RequestBody Dog dog) {
-        if (id >= 0 && id < dogs.size()) {
-            dogs.set(id, dog);
-            return new ResponseEntity<>(dog, HttpStatus.OK);
-        }
-        return new ResponseEntity<>("Invalid id", HttpStatus.BAD_REQUEST);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> editDog(@PathVariable long id, @RequestBody Dog dog) throws Exception {
+
+            Dog updateDog = dogRepository.findById(id).orElseThrow(() -> new Exception("Dog does not exist with id: " + id));
+            updateDog.setName(dog.getName());
+            updateDog.setBreedGroup(dog.getBreedGroup());
+            updateDog.setBreed(dog.getBreed());
+            updateDog.setChipnumber(dog.getChipnumber());
+            updateDog.setLitter(dog.getLitter());
+
+            dogRepository.save(updateDog);
+
+            return ResponseEntity.ok(updateDog.getName());
     }
 
     //Delete mapping voor de hond met het opgegeven id in de database
-    @DeleteMapping("/dogs/{id}")
-    public ResponseEntity<Object> removeDog(@PathVariable int id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> removeDog(@PathVariable long id) {
         {
-            if(id < dogs.size()){
-                dogs.remove(id);
-                return new ResponseEntity<>("Removed!", HttpStatus.OK);
+            if(dogRepository.existsById(id)){
+                dogRepository.deleteById(id);
+                return ResponseEntity.ok("Dog deleted from database.");
             }else {
-                return new ResponseEntity<>("Id is not found in database", HttpStatus.BAD_REQUEST);
+                return ResponseEntity.notFound().build();
             }
         }
     }
--- breedsoft/src/main/java/nl/novi/breedsoft/repository/DogRepository.java
+++ breedsoft/src/main/java/nl/novi/breedsoft/repository/DogRepository.java
@@ -3,5 +3,5 @@ import org.springframework.data.repository.CrudRepository;
 import nl.novi.breedsoft.model.animal.Dog;
 
 public interface DogRepository extends CrudRepository<Dog, Long>{
-
+    public Iterable<Dog> findByNameContaining(String name);
 }
