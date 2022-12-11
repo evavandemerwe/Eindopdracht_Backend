062d5e72bbc307be2a63215eac2f963db600c123
evavandemerwe <evandemerwe@outlook.com>
2022-12-11 14:58:46 +0100

    - Added Lombok - Updated DTOs

--- breedsoft/src/main/java/nl/novi/breedsoft/dto/DogOutputDto.java
+++ breedsoft/src/main/java/nl/novi/breedsoft/dto/DogOutputDto.java
@@ -1,10 +1,12 @@
 package nl.novi.breedsoft.dto;
 
+import lombok.Data;
 import nl.novi.breedsoft.model.animal.enumerations.Breed;
 import nl.novi.breedsoft.model.animal.enumerations.BreedGroup;
 
 import java.util.ArrayList;
 
+@Data
 public class DogOutputDto {
     private long id;
     private String name;
@@ -13,60 +15,4 @@ public class DogOutputDto {
     private int dogYears;
     private ArrayList<String> Litter;
     private BreedGroup breedGroup;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getChipnumber() {
-        return chipnumber;
-    }
-
-    public void setChipnumber(String chipnumber) {
-        this.chipnumber = chipnumber;
-    }
-
-    public Breed getBreed() {
-        return breed;
-    }
-
-    public void setBreed(Breed breed) {
-        this.breed = breed;
-    }
-
-    public int getDogYears() {
-        return dogYears;
-    }
-
-    public void setDogYears(int dogYears) {
-        this.dogYears = dogYears;
-    }
-
-    public ArrayList<String> getLitter() {
-        return Litter;
-    }
-
-    public void setLitter(ArrayList<String> litter) {
-        Litter = litter;
-    }
-
-    public BreedGroup getBreedGroup() {
-        return breedGroup;
-    }
-
-    public void setBreedGroup(BreedGroup breedGroup) {
-        this.breedGroup = breedGroup;
-    }
 }
