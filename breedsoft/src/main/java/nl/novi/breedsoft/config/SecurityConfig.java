65fb1d144629669a824106125d3d9f629dfa4fa9
evavandemerwe <evandemerwe@outlook.com>
2022-12-23 19:46:08 +0100

    - Adding security for spring boot 3.0

--- breedsoft/src/main/java/nl/novi/breedsoft/config/SecurityConfig.java
+++ breedsoft/src/main/java/nl/novi/breedsoft/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package nl.novi.breedsoft.config;
 
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpMethod.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,9 +19,14 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import java.security.interfaces.RSAKey;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private RSAKey rsaKey;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -26,40 +34,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        InMemoryUserDetailsManager man = new InMemoryUserDetailsManager();
-
-        UserDetails u1 = User
-                .withUsername("gebruiker01")
-                .password(encoder.encode("Gebruiker2022"))
-                .roles("USER")
-                .build();
-        man.createUser(u1);
-
-        UserDetails u2 = User
-                .withUsername("fokker01")
-                .password(encoder.encode("Fokker2022"))
-                .roles("ADMIN")
-                .build();
-        man.createUser(u2);
-
-        return man;
+    InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(User.withUsername("User")
+                .password("{noop}password")
+                .authorities("read")
+                .build());
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                .disable()
-                .cors()
-                .disable()
-                .httpBasic()
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/**").hasAnyRole("USER", "ADMIN")
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/token").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults())
+                .build();
+    }
 
-        return http.build();
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        rsaKey = Jwks.ge
+
     }
 }
