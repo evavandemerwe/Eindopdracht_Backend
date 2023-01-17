229b804481da90b57eb90ae1f65da57e5b17ff3a
EvadeJong <Eefjuh.de.jong@gmail.com>
2023-01-17 20:23:06 +0100

    added javadoc to exception classes

--- breedsoft/src/main/java/nl/novi/breedsoft/exception/BadFileException.java
+++ breedsoft/src/main/java/nl/novi/breedsoft/exception/BadFileException.java
@@ -1,10 +1,16 @@
 package nl.novi.breedsoft.exception;
 
+import java.io.Serial;
+
 public class BadFileException extends RuntimeException{
+
+    @Serial
     private static final long serialVersionUID = 1L;
-    public BadFileException() {
-        super();
-    }
+
+    /**
+     * Exception method with a form of Throwable that indicates conditions that might need to be caught
+     * @param message The message given with the thrown exception
+     */
     public BadFileException(String message) {
         super(message);
     }
