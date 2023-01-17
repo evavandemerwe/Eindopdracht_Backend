f51520d5238258f9d2a7a7487f20febef6393d41
EvadeJong <Eefjuh.de.jong@gmail.com>
2023-01-17 20:32:38 +0100

    added javadoc to repo classes

--- breedsoft/src/main/java/nl/novi/breedsoft/repository/DomesticatedDogRepository.java
+++ breedsoft/src/main/java/nl/novi/breedsoft/repository/DomesticatedDogRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DomesticatedDogRepository extends JpaRepository<DomesticatedDog, Long> {
+    /**
+     * A repository for retrieval of domesticated dogs
+     * @param name which is requested to be searched for in the database
+     * @return an Optional DomesticatedDog - DomesticatedDog is returned if found
+     */
     List<DomesticatedDog> findByNameContaining(String name);
 }
