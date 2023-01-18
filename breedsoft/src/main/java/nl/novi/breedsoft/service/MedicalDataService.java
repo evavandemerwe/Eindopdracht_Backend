36a7db39d641e14a9ffd43ad87ce888832023a90
EvadeJong <Eefjuh.de.jong@gmail.com>
2023-01-18 11:03:37 +0100

    added javadoc to medical data service

--- breedsoft/src/main/java/nl/novi/breedsoft/service/MedicalDataService.java
+++ breedsoft/src/main/java/nl/novi/breedsoft/service/MedicalDataService.java
@@ -24,19 +24,28 @@ public class MedicalDataService {
         this.domesticatedDogRepository = domesticatedDogRepository;
     }
 
+    /**
+     * A method for retrieval of all medical data from the database
+     * @return a list of medical data in output dto format
+     */
     public List<MedicalDataOutputDto> getAllMedicalData(){
         List<MedicalData> allMedicalDataList = medicalDataRepository.findAll();
-        List<MedicalDataOutputDto> medicalDataOutputDtoList = transferMedicalDataToOutputDtoList(allMedicalDataList);
-        return medicalDataOutputDtoList;
+        return transferMedicalDataToOutputDtoList(allMedicalDataList);
     }
 
-    public List<MedicalDataOutputDto> getMedicaLDataByDogId(Long id){
+    /**
+     * A method for retrieval of one medical data record from the database by id
+     * @param domesticatedDogId ID of the dog for which information is requested
+     * @return medical data in output dto format
+     * @throws RecordNotFoundException throws an exception when no medical data is found for dog with given id
+     */
+    public List<MedicalDataOutputDto> getMedicaLDataByDogId(Long domesticatedDogId){
         List<MedicalData> allMedicalData = medicalDataRepository.findAll();
         List<MedicalData> dogMedicalData = new ArrayList<>();
 
         for(MedicalData medicalData : allMedicalData){
             Long dogId = medicalData.getDomesticatedDog().getId();
-            if(dogId.equals(id)){
+            if(dogId.equals(domesticatedDogId)){
                 dogMedicalData.add(medicalData);
             }
         }
@@ -46,6 +55,12 @@ public class MedicalDataService {
         return transferMedicalDataToOutputDtoList(dogMedicalData);
     }
 
+    /**
+     * A method to create a new medical data record in the database
+     * @param medicalDataInputDto Data Transfer Objects that carries data between processes in order to reduce the number of methods calls
+     * @return the ID of the medical data found in the database
+     * @throws RecordNotFoundException throws an exception when the provided dog does not exist
+     */
     public Long createMedicalData(MedicalDataInputDto medicalDataInputDto){
         //Check if a dog is given
         if(medicalDataInputDto.getDomesticatedDog() != null){
@@ -61,12 +76,18 @@ public class MedicalDataService {
         return medicalData.getId();
     }
 
-    //PUT sends an enclosed entity of a resource to the server.
-    //If the entity already exists, the server overrides the existing object.
-    //Otherwise, the server creates a new entity
-    public Object updateMedicalData(Long id, MedicalDataInputDto medicalDataInputDto){
-        if(medicalDataRepository.findById(id).isPresent()){
-            MedicalData medicalData = medicalDataRepository.findById(id).get();
+    /**
+     * A method (PUT) sends an enclosed entity of a resource to the server.
+     * If the entity already exists, the server overrides the existing object,
+     * otherwise the server creates a new entity.
+     * @param medicalDataId ID of the medical data for which an update or creation is requested
+     * @param medicalDataInputDto Data Transfer Objects that carries data between processes in order to reduce the number of methods calls
+     * @return a new or updated medical data record in output dto format
+     * @throws RecordNotFoundException throws an exception when domesticated dog is not found by id
+     */
+    public Object updateMedicalData(Long medicalDataId, MedicalDataInputDto medicalDataInputDto){
+        if(medicalDataRepository.findById(medicalDataId).isPresent()){
+            MedicalData medicalData = medicalDataRepository.findById(medicalDataId).get();
             MedicalData updateMedicalData = transferToMedicalData(medicalDataInputDto);
             if(medicalDataInputDto.getDomesticatedDog() != null) {
                 DomesticatedDog dog = getCompleteDogById(medicalDataInputDto.getDomesticatedDog().getId());
@@ -87,11 +108,19 @@ public class MedicalDataService {
         }
     }
 
-    //PATCH will only update an existing object,
-    //with the properties mapped in the request body (that are not null).
-    public MedicalDataOutputDto patchMedicalData(Long id, MedicalDataPatchDto medicalDataPatchDto){
-        if(medicalDataRepository.findById(id).isPresent()){
-            MedicalData updateMedicalData = medicalDataRepository.getReferenceById(id);
+    /**
+     * A method (PATCH) will only update an existing object,
+     * with the properties mapped in the request body (that are not null).
+     * We do NOT update veterinarian appointment and medical data here.
+     * @param medicalDataId ID of the medical data for which an update is requested
+     * @param medicalDataPatchDto Data Transfer Objects that carries data between processes in order to reduce the number of methods calls
+     * @return the updated medical data in output dto format
+     * @throws RecordNotFoundException throws an exception when provided domesticated dog does not exist,
+     * or when medical data is not found based on medicalDataId
+     */
+    public MedicalDataOutputDto patchMedicalData(Long medicalDataId, MedicalDataPatchDto medicalDataPatchDto){
+        if(medicalDataRepository.findById(medicalDataId).isPresent()){
+            MedicalData updateMedicalData = medicalDataRepository.getReferenceById(medicalDataId);
             if(medicalDataPatchDto.getDateOfMedicalTreatment() != null){
                 updateMedicalData.setDateOfMedicalTreatment(medicalDataPatchDto.getDateOfMedicalTreatment());
             }
@@ -121,16 +150,21 @@ public class MedicalDataService {
         }
     }
 
-    public void deleteMedicalData(Long id){
+    /**
+     * A method for deleting medical data from the database by id
+     * @param medicalDataId ID of the medical data for which information is requested
+     * @throws RecordNotFoundException throws an exception if no medical data is found by medicalDataId
+     */
+    public void deleteMedicalData(Long medicalDataId){
 
-        if(medicalDataRepository.findById(id).isPresent()){
+        if(medicalDataRepository.findById(medicalDataId).isPresent()){
             // If an appointment is deleted from the database, this appointment is detached from dogs.
-            MedicalData medicalDataFound = medicalDataRepository.getReferenceById(id);
+            MedicalData medicalDataFound = medicalDataRepository.getReferenceById(medicalDataId);
 
             if(medicalDataFound.getDomesticatedDog() != null){
                 medicalDataFound.setDomesticatedDog(null);
             }
-            medicalDataRepository.deleteById(id);
+            medicalDataRepository.deleteById(medicalDataId);
         } else {
             throw new RecordNotFoundException("No medical data with given ID found.");
         }
@@ -138,6 +172,11 @@ public class MedicalDataService {
 
     //DTO Helper classes
 
+    /**
+     * A method to transform medical data in input dto format to medical data format
+     * @param medicalDataInputDto Data Transfer Objects that carries data between processes in order to reduce the number of methods calls
+     * @return Medical data
+     */
     private MedicalData transferToMedicalData(MedicalDataInputDto medicalDataInputDto){
         MedicalData medicalData = new MedicalData();
 
@@ -151,14 +190,26 @@ public class MedicalDataService {
 
         return medicalData;
     }
-    private List<MedicalDataOutputDto> transferMedicalDataToOutputDtoList(List <MedicalData> medicalData){
+
+    /**
+     * A method to transform medical data list to medical data list in output dto format
+     * @param medicalDataList a list with medical data in medical data format
+     * @return a list with medical data in output dto format
+     */
+    private List<MedicalDataOutputDto> transferMedicalDataToOutputDtoList(List <MedicalData> medicalDataList){
         List<MedicalDataOutputDto> medicalDataOutputDtoList = new ArrayList<>();
-        for(MedicalData data : medicalData){
+        for(MedicalData data : medicalDataList){
             MedicalDataOutputDto dto = transferMedicalDataToOutputDto(data);
             medicalDataOutputDtoList.add(dto);
         }
         return medicalDataOutputDtoList;
     }
+
+    /**
+     * A method to transform medical data to medical data in output dto format
+     * @param medicalData medical data in medical data format
+     * @return medical data in output dto format
+     */
 
     private MedicalDataOutputDto transferMedicalDataToOutputDto(MedicalData medicalData){
         MedicalDataOutputDto medicalDataOutputDto = new MedicalDataOutputDto();
@@ -172,11 +223,14 @@ public class MedicalDataService {
         return medicalDataOutputDto;
     }
 
-
-    //Look for dog by ID in dogrespository.
-    //When nu dog ID is given, the get dog method returns 0 and an error is thrown.
-    //When dog ID is found, dog is returned. If there is no dog found in the repository, null is returned.
-
+    /**
+     * Look for domesticated dog by id in domesticated dog repository.
+     * When no domesticated dog ID is given, the get domesticated dog method returns 0 and an error is thrown.
+     * When domesticated dog ID is found, domesticated dog is returned.
+     * If there is no domesticated dog found in the repository, null is returned.
+     * @param dogId ID of the domesticated dog for which information is requested
+     * @return domesticated dog or null if not present.
+     */
     private DomesticatedDog getCompleteDogById(Long dogId){
         if(dogId == 0){
             throw new RecordNotFoundException("Missing dog ID");
