755010fe6561a35f1a813037deaf15285c52801b
evavandemerwe <evandemerwe@outlook.com>
2023-01-26 21:05:28 +0100

    Final UserService tests

--- breedsoft/src/test/java/nl/novi/breedsoft/service/UserServiceTest.java
+++ breedsoft/src/test/java/nl/novi/breedsoft/service/UserServiceTest.java
@@ -1,6 +1,8 @@
 package nl.novi.breedsoft.service;
 
+import nl.novi.breedsoft.dto.userDtos.UserInputDto;
 import nl.novi.breedsoft.dto.userDtos.UserOutputDto;
+import nl.novi.breedsoft.dto.userDtos.UserPatchDto;
 import nl.novi.breedsoft.model.authority.User;
 import nl.novi.breedsoft.repository.AuthorityRepository;
 import nl.novi.breedsoft.repository.UserRepository;
@@ -8,14 +10,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -57,13 +62,55 @@ class UserServiceTest {
 
     @Test
     void createUser() {
+        // Arrange
+        UserInputDto userInputDto = new UserInputDto();
+        String expectedUsername = "expectedUsername";
+        userInputDto.setUsername(expectedUsername);
+        String expectedPassword = "expectedPassword";
+        userInputDto.setPassword(expectedPassword);
+        userInputDto.setAuthorities(new ArrayList<>());
+        when(encoder.encode(Mockito.anyString())).thenAnswer(i -> i.getArguments()[0]);
+        final AtomicReference<User> userAtomicReference = new AtomicReference<>() ;
+        when(userRepository.save(Mockito.any(User.class))).then(
+                i -> {
+                    userAtomicReference.set((User)i.getArguments()[0]);
+                    return i.getArguments()[0];
+                }
+        );
+        // Act
+        userService.createUser(userInputDto);
+        // Assert
+        User storedMedicalData = userAtomicReference.get();
+        assertEquals(expectedUsername, storedMedicalData.getUsername());
+        assertEquals(expectedPassword, storedMedicalData.getPassword());
+        verify(userRepository, times(1)).save(Mockito.any(User.class));
     }
 
     @Test
     void patchUser() {
+        // Arrange
+        UserPatchDto userPatchDto = new UserPatchDto();
+        String expectedUsername = "expectedUsername";
+        userPatchDto.setUsername(expectedUsername);
+        User user = new User();
+        when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(user));
+        when(userRepository.getReferenceById(Mockito.anyLong())).thenReturn(user);
+        // Act
+        UserOutputDto result = userService.patchUser(42L, userPatchDto);
+        // Assert
+        verify(userRepository, times(1)).save(user);
+        assertEquals(expectedUsername, user.getUsername());
+        assertEquals(expectedUsername, result.getUsername());
     }
 
     @Test
     void deleteUser() {
+        // Arrange
+        Long id = 42L;
+        when(userRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(new User()));
+        // Act
+        userService.deleteUser(id);
+        // Assert
+        verify(userRepository, times(1)).deleteById(id);
     }
 }
